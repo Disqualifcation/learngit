4218f85e2b2d560d8b8b802142fba8cd8af8e60e
LENOVO <2496504534@qq.com>
2022-07-07 16:23:59 +0800

    add some object

---
+++ mbgz.py
@@ -0,0 +1,88 @@
+
+
+
+import cv2
+import sys
+
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+
+if __name__ == '__main__':
+
+
+#建立追踪器
+    tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
+    tracker_type = tracker_types[2]
+
+    if int(minor_ver) < 3:
+        tracker = cv2.Tracker_create(tracker_type)
+    else:
+        if tracker_type == 'BOOSTING':
+            tracker = cv2.TrackerBoosting_create()
+        if tracker_type == 'MIL':
+            tracker = cv2.TrackerMIL_create()
+        if tracker_type == 'KCF':
+            tracker = cv2.TrackerKCF_create()
+        if tracker_type == 'TLD':
+            tracker = cv2.TrackerTLD_create()
+        if tracker_type == 'MEDIANFLOW':
+            tracker = cv2.TrackerMedianFlow_create()
+        if tracker_type == 'GOTURN':
+            tracker = cv2.TrackerGOTURN_create()
+
+    #设置变量video为视频
+    video = cv2.VideoCapture("los_angeles.mp4")
+
+
+    # 读取视频
+    ok, frame = video.read()
+    if not ok:
+        print('Cannot read video file')
+        sys.exit()
+
+    # 定义初始化边框
+    box = (287, 40, 86, 320)
+
+    #用于对目标的框选
+    box = cv2.selectROI(frame, False)
+
+    # 使用目标跟踪器跟踪框选的
+    ok = tracker.init(frame, box)
+
+    while True:
+        # 读取框选时刻
+        ok, frame = video.read()
+        if not ok:
+            break
+
+        # 计算执行时间
+        timer = cv2.getTickCount()
+
+        # 追踪结果
+        ok, bbox = tracker.update(frame)
+
+
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+
+        # 绘制边框
+        if ok:
+            # 追踪器运行，通过实时追踪rectangle绘制矩形边框
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+        else:
+            #如果无边框输出则追踪器完成追踪
+            cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+
+        #
+        cv2.putText(frame, tracker_type + " Tracker", (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+
+
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+
+        # 展示追踪结果
+        cv2.imshow("Tracking", frame)
+
+        # 按下esc键退出
+        k = cv2.waitKey(1) & 0xff
+        if k == 27:
+            break
---
+++ wtcl.py
@@ -0,0 +1,73 @@
+#对实时物体进行边缘检测··
+from scipy.spatial import distance as dist
+from imutils import perspective
+from imutils import contours
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+#定义两个变量，width用于将尺寸转换
+pix = None
+width=2
+#读取图像，转换图像为灰度图像，并高斯模糊
+img = cv2.imread("20201015152437717.png")
+cv2.imshow("img",img)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+r=cv2.GaussianBlur(gray,(7,7),0)
+
+#边缘检测
+edged = cv2.Canny(r, 50, 100)
+kernel = np.ones((5,5),dtype=np.uint8)
+edged = cv2.morphologyEx(edged,cv2.MORPH_CLOSE,kernel)
+
+#轮廓查找
+refCnts, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+#计算最小外接矩形
+for (i, c) in enumerate(refCnts):
+    #将大小过小的轮廓舍弃
+    if cv2.contourArea(c) <200:
+        continue
+    rect = cv2.minAreaRect(c)
+    points = cv2.boxPoints(rect)
+    points =np.int0(points)
+    points =perspective.order_points(points)
+    cv2.drawContours(img, [points.astype("int")], -1, (0, 0, 255), 2)
+    #对最小外接矩形的坐标计算
+    (tl, tr, br, bl) = points
+    #为计算尺寸运用公式先计算中点坐标
+    (tltrX, tltrY) = (tl[0] + tr[0]) * 0.5, (tl[1] + tr[1]) * 0.5
+    (blbrX, blbrY) = (bl[0] + br[0]) * 0.5, (bl[1] + br[1]) * 0.5
+    (tlblX, tlblY) = (tl[0] + bl[0]) * 0.5, (tl[1] + bl[1]) * 0.5
+    (trbrX, trbrY) = (tr[0] + br[0]) * 0.5, (tr[1] + br[1]) * 0.5
+
+    # 分别计算中心点坐标
+    cv2.circle(img, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+    cv2.circle(img, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+    cv2.circle(img, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+    cv2.circle(img, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+
+    # 计算两个中心点之间的欧氏距离，即图片距离
+    dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+    dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+
+    # 初始化测量指标值
+    if pix is None:
+        pix = dB / width
+
+    # 计算目标的实际大小（宽和高），用英尺来表示
+    dimA = dA / pix
+    dimB = dB / pix
+
+    # 在图片中绘制结果
+    cv2.putText(img, "{:.1f}in".format(dimB),
+                (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+                0.65, (255, 255, 255), 2)
+    cv2.putText(img, "{:.1f}in".format(dimA),
+                (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
+                0.65, (255, 255, 255), 2)
+
+cv2.imshow("image",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
